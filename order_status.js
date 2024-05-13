539424900cc04c5aa363a253c07e72c2348c5bdf
Yash Verma <yashmacbook@Yashs-MacBook-Air.local>
2024-05-13 18:12:47 +0530

    add elemet

--- order_status.js
+++ order_status.js
@@ -1,8 +1,11 @@
 Shopify.Checkout.OrderStatus.addContentBox(`tailboost`);
 
 const newElement = document.createElement("div");
-newElement.innerHTML =
-  "<h3>Special Announcement</h3><p>This is a special promotion just for you!</p>";
+newElement.innerHTML = `
+  <html>
+  <h3>Special Announcement</h3>
+  <p>This is a special promotion just for you!</p>
+  </html>`;
 newElement.className = "special-announcement";
 
 const targetDiv = document.querySelector(".main");
