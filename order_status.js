fe7d23f60c2f257890ea1cb6e534079d3061e211
Yash Verma <yashmacbook@Yashs-MacBook-Air.local>
2024-05-13 17:41:24 +0530

    add elemet

--- order_status.js
+++ order_status.js
@@ -1,1 +1,13 @@
-Shopify.Checkout.OrderStatus.addContentBox(`tailboost`)
+Shopify.Checkout.OrderStatus.addContentBox(`tailboost`);
+
+const newElement = document.createElement("div");
+newElement.innerHTML =
+  "<h3>Special Announcement</h3><p>This is a special promotion just for you!</p>";
+newElement.className = "special-announcement";
+
+const targetDiv = document.querySelector(".main");
+if (targetDiv) {
+  targetDiv.appendChild(newElement);
+} else {
+  console.error('The div with class "main" was not found.');
+}
