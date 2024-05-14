d8a4fff6da9b8a78e2fbafaae1ca024292372126
Yash <yash.verma@tailboost.ai>
2024-05-14 14:28:53 +0530

    add new orderstatus script

---
+++ order_status_new.js
@@ -0,0 +1,26 @@
+async function hit() {
+  const data = await fetch(
+    window.location.origin +
+      "/apps/proxy/?shop=" +
+      window.location.host +
+      "&ip=" +
+      ipData.ip +
+      "&current_url=" +
+      window.location.href
+  );
+
+  const newElement = document.createElement("div");
+
+  newElement.innerHTML = await data.text();
+
+  newElement.className = "special-announcement";
+
+  const targetDiv = document.querySelector(".main");
+  if (targetDiv) {
+    targetDiv.prepend(newElement);
+  } else {
+    console.error('The div with class "main" was not found.');
+  }
+}
+
+hit();
