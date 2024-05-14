4529602d48ab4ccbc2ae04cd04ade1b4154bc387
Yash <yash.verma@tailboost.ai>
2024-05-14 17:27:56 +0530

    add log user script

--- loguser.js
+++ loguser.js
@@ -1,1 +1,24 @@
-console.log(window)
+console.log(window);
+
+async function fetchIpAddress() {
+  const data = await fetch("https://api64.ipify.org?format=json");
+  const res = await data.json();
+  console.log(res);
+  return res;
+}
+
+async function hit() {
+  const ipData = await fetchIpAddress();
+  const data = await fetch(
+    window.location.origin +
+      "/apps/proxy/?shop=" +
+      window.location.host +
+      "&ip=" +
+      ipData.ip +
+      "&current_url=" +
+      window.location.href
+  );
+  console.log(data);
+}
+
+hit();
