22319ffd38712a66174556c373c0d0d10b7f35c0
Yash <yash.verma@tailboost.ai>
2024-05-14 14:39:16 +0530

    fix hit

--- order_status.js
+++ order_status.js
@@ -1,20 +1,20 @@
 Shopify.Checkout.OrderStatus.addContentBox(`tailboost`);
 
-const newElement = document.createElement("div");
-newElement.innerHTML = `
-  <html>
-  <h3>Special Announcement</h3>
-  <p>This is a special promotion just for you!</p>
-  </html>`;
+// const newElement = document.createElement("div");
+// newElement.innerHTML = `
+//   <html>
+//   <h3>Special Announcement</h3>
+//   <p>This is a special promotion just for you!</p>
+//   </html>`;
 
-newElement.className = "special-announcement";
+// newElement.className = "special-announcement";
 
-const targetDiv = document.querySelector(".main");
-if (targetDiv) {
-  targetDiv.prepend(newElement);
-} else {
-  console.error('The div with class "main" was not found.');
-}
+// const targetDiv = document.querySelector(".main");
+// if (targetDiv) {
+//   targetDiv.prepend(newElement);
+// } else {
+//   console.error('The div with class "main" was not found.');
+// }
 
 async function hit() {
   const data = await fetch(
@@ -32,15 +32,15 @@ async function hit() {
     }
   );
 
-  const newElement = document.createElement("div");
+  const tailboostElement = document.createElement("div");
 
-  newElement.innerHTML = await data.text();
+  tailboostElement.innerHTML = await data.text();
 
-  newElement.className = "special-announcement-2";
+  tailboostElement.className = "special-announcement-2";
 
   const targetDiv = document.querySelector(".main");
   if (targetDiv) {
-    targetDiv.prepend(newElement);
+    targetDiv.prepend(tailboostElement);
   } else {
     console.error('The div with class "main" was not found.');
   }
